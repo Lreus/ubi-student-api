58bf3ecc4d55e82a8bcb05e68321ae061b17876d
Lreus <ludovic.reus@free.fr>
2020-12-19 07:38:12 +0100

    Test accepted and bad request response by mocking studentRepository

---
+++ tests/Students/Api/CreateStudentTest.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Students\Api;
+
+use App\Controller\Student\PostController;
+use App\Entity\Student;
+use App\Exception\ValidationException;
+use App\Repository\StudentRepository;
+use DateTimeImmutable;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateStudentTest extends WebTestCase
+{
+    /**
+     * @var KernelBrowser
+     */
+    private $client;
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+    }
+
+    public function testCreatedStudentGenerateCreatedResponse()
+    {
+        $mock = $this->startStudentRepositoryMock();
+
+        $expectedStudent = new Student(
+            'anyId',
+            'Doe',
+            'John',
+            new DateTimeImmutable()
+        );
+
+        $mock->method('createFromRequest')->willReturn($expectedStudent);
+
+        $client = $this->postStudent($this->getValidStudentValues());
+
+        $response = $client->getResponse();
+        $this->assertStringContainsString('application/json', $response->headers->get('Content-type'));
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+
+        $decodedResponse = $this->decodeResponse($client);
+        $this->assertSame($expectedStudent->getId(), $decodedResponse['id']);
+    }
+
+    public function testValidationExceptionReturnsBadRequest()
+    {
+        $mock = $this->startStudentRepositoryMock();
+
+        $mock->method('createFromRequest')->willThrowException(new ValidationException());
+
+        $this->client = $this->postStudent($this->getValidStudentValues());
+
+        $response = $this->decodeResponse($this->client);
+        $this->assertSame(
+            PostController::BAD_REQUEST_MESSAGE,
+            $response['message']
+        );
+    }
+
+    private function startStudentRepositoryMock(): MockObject
+    {
+        $mock = $this->getMockBuilder(StudentRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->client->getContainer()->set(StudentRepository::class, $mock);
+
+        return $mock;
+    }
+
+    private function getValidStudentValues(): array
+    {
+        return [
+            'first_name' => 'Ludovic',
+            'last_name' => 'REUS',
+            'birth_date' => '07/01/1982',
+        ];
+    }
+
+    private function decodeResponse(KernelBrowser $client): array
+    {
+        $response = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($response);
+
+        return $response;
+    }
+
+    private function postStudent(array $postParameters): KernelBrowser
+    {
+        $this->client->request(
+            'PUT',
+            '/api/student',
+            [],
+            [],
+            [
+                'HTTP_Accept' => 'application/json'
+            ],
+            json_encode($postParameters)
+        );
+
+        return $this->client;
+    }
+}
