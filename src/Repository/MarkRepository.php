303b3aa315e02b525c300d557968c3d8c60e2c8f
Lreus <ludovic.reus@free.fr>
2020-12-20 16:08:26 +0100

    handle validation and mark creation from request

--- src/Repository/MarkRepository.php
+++ src/Repository/MarkRepository.php
@@ -4,7 +4,54 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Constraint\ValidMarkConstraint;
+use App\Entity\Mark;
+use App\Entity\Student;
+use App\Exception\ValidationException;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class MarkRepository
 {
+    private ValidatorInterface $validator;
 
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function createFromRequest(array $content, Student $student)
+    {
+        $constraint = new Assert\Collection([
+            'fields' => [
+                'value' => [
+                    new Assert\Required([]),
+                    new ValidMarkConstraint(),
+                ],
+                'subject' => [
+                    new Assert\Required([]),
+                    new Assert\Type([
+                        'type' => 'string',
+                    ]),
+                    new Assert\NotBlank([
+                        'normalizer' => 'trim'
+                        ]
+                    ),
+                ],
+            ],
+        ]);
+
+        $violationList = $this->validator->validate($content, $constraint);
+        if (0 < count($violationList)) {
+            throw new ValidationException($violationList);
+        }
+
+        return new Mark(
+            Uuid::uuid4()->toString(),
+            floatval($content['value']),
+            $content['subject'],
+            $student
+        );
+    }
 }
