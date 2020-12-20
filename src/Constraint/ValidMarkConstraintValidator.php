0bed4fe2fdfcece898406bd322e98af3ae249b62
Lreus <ludovic.reus@free.fr>
2020-12-20 15:51:12 +0100

    allow numeric string and handle round and range

--- src/Constraint/ValidMarkConstraintValidator.php
+++ src/Constraint/ValidMarkConstraintValidator.php
@@ -10,16 +10,37 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 class ValidMarkConstraintValidator extends ConstraintValidator
 {
+    const NOT_FLOAT = 'notFloat';
+    const OUT_OF_RANGE = 'outOfRange';
+    const NOT_ROUNDED = 'notRounded';
+
     public function validate($value, Constraint $constraint)
     {
         if (!($constraint instanceof ValidMarkConstraint)) {
             throw new UnexpectedTypeException($constraint, ValidMarkConstraint::class);
         }
 
-        if (!is_float($value)) {
-            $this->context->buildViolation($constraint->message)
-                ->setParameter('{{ string }}', (string) $value)
-                ->addViolation();
+        if (!is_numeric($value)) {
+            $this->addViolation($value, $constraint, self::NOT_FLOAT);
         }
+
+        if(is_numeric($value)) {
+            $floatValue = floatval($value);
+
+            if(0 > $floatValue || 20 < $floatValue ) {
+                $this->addViolation($value, $constraint, self::OUT_OF_RANGE);
+            }
+
+            if ($floatValue !== round($floatValue, 2)) {
+                $this->addViolation($value, $constraint, self::NOT_ROUNDED);
+            }
+        }
+    }
+
+    private function addViolation($value, ValidMarkConstraint $constraint, string $reason)
+    {
+        $this->context->buildViolation($constraint->getMessage($reason))
+            ->setParameter('{{ string }}', (string) $value)
+            ->addViolation();
     }
 }
