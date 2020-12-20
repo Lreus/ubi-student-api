65fbde7d1fc7a993b4d8fcce7dc023942597b5e1
Lreus <ludovic.reus@free.fr>
2020-12-20 10:41:15 +0100

    Remove useless declaration

--- tests/Repository/StudentRepositoryTest.php
+++ tests/Repository/StudentRepositoryTest.php
@@ -5,15 +5,9 @@ declare(strict_types=1);
 namespace App\Tests\Repository;
 
 use App\Entity\Student;
-use App\Exception\ValidationException;
 use App\Repository\StudentRepository;
 use App\Tests\Repository\Student\AbstractStudentRepositoryTest;
 use DateTimeImmutable;
-use Doctrine\Bundle\DoctrineBundle\Registry;
-use Doctrine\ORM\EntityNotFoundException;
-use Doctrine\Persistence\ObjectManager;
-use Iterator;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class StudentRepositoryTest extends AbstractStudentRepositoryTest
 {
