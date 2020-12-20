ff95a65739c7a6bf43ae0f4eaa840cde12ba7675
Lreus <ludovic.reus@free.fr>
2020-12-20 10:34:23 +0100

    Split tests into feature for readability

---
+++ tests/Repository/Student/AbstractStudentRepositoryTest.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repository\Student;
+
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Persistence\ObjectManager;
+use Iterator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class AbstractStudentRepositoryTest extends KernelTestCase
+{
+    protected StudentRepository $subject;
+
+    protected ObjectManager $studentEntityManager;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $this->subject = $this->getStudentRepository();
+
+        $this->studentEntityManager = $this->getEntityManager();
+    }
+
+    private function getEntityManager(): ObjectManager
+    {
+        $doctrine = self::$container->get('doctrine');
+        $this->assertInstanceOf(Registry::class, $doctrine);
+
+        /** @var Registry $doctrine */
+        $em = $doctrine->getManager();
+        $this->assertInstanceOf(ObjectManager::class, $em);
+
+        return $em;
+    }
+
+    private function getStudentRepository(): StudentRepository
+    {
+        $repository = self::$container->get(StudentRepository::class);
+        $this->assertInstanceOf(StudentRepository::class, $repository);
+
+        /** @var StudentRepository $repository */
+        return $repository;
+    }
+
+    protected function clearStudentFromDatabase(string $studentId): void
+    {
+        $existing = $this->studentEntityManager->find(Student::class, $studentId);
+        if (null === $existing) {
+            return;
+        }
+
+        $this->studentEntityManager->remove($existing);
+        $this->studentEntityManager->flush();
+        $this->assertNull($this->studentEntityManager->find(Student::class, $studentId));
+    }
+
+    public function invalidStudentProvider(): Iterator
+    {
+        // empty array
+        yield [
+            []
+        ];
+
+        // missing or misspelled fields
+        yield [
+            [
+                'first_name' => 'Ludovic',
+                'birth_date' => '07/01/1982',
+            ]
+        ];
+
+        yield [
+            [
+                'last_name' => 'REUS',
+                'birth_date' => '07/01/1982',
+            ]
+        ];
+
+        yield [
+            [
+                'first_name' => 'Ludovic',
+                'last_name' => 'REUS',
+            ]
+        ];
+
+        // empty values
+        yield [
+            [
+                'first_name' => '  ',
+                'last_name' => 'REUS',
+                'birth_date' => '07/01/1982',
+            ]
+        ];
+
+        yield [
+            [
+                'first_name' => 'Ludovic',
+                'last_name' => '  ',
+                'birth_date' => '07/01/1982',
+            ]
+        ];
+
+        // Invalid format Date
+        yield [
+            [
+                'first_name' => 'Ludovic',
+                'last_name' => 'REUS',
+                'birth_date' => '07-01-1982',
+            ]
+        ];
+    }
+
+    protected function tearDown(): void
+    {
+        self::ensureKernelShutdown();
+    }
+}
---
+++ tests/Repository/Student/UpdateStudentTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repository\Student;
+
+use App\Entity\Student;
+use App\Exception\ValidationException;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityNotFoundException;
+
+class UpdateStudentTest extends AbstractStudentRepositoryTest
+{
+    /**
+     * @dataProvider invalidStudentProvider
+     */
+    public function testInvalidDataThrowsValidationExceptionOnUpdate(array $studentValues)
+    {
+        $this->expectException(ValidationException::class);
+
+        $this->subject->updateFromRequest($studentValues, 'anyId');
+    }
+
+    public function testUnknownUserThrowEntityNotFoundException()
+    {
+        $userId = 'the_existing_user';
+        $this->clearStudentFromDatabase($userId);
+
+        $updatedContent = [
+            'first_name' => 'Thierry',
+            'last_name' => 'Lebon',
+            'birth_date' => '06/02/1983',
+        ];
+
+        $this->expectException(EntityNotFoundException::class);
+
+        $this->subject->updateFromRequest($updatedContent, $userId);
+    }
+
+    public function testUpdateFromRequest()
+    {
+        $userId = 'the_existing_user';
+        $userLastName = 'Doe';
+        $userFirstName = 'John';
+        $userBirthDate = DateTimeImmutable::createFromFormat('d/m/Y', '02/12/1982');
+
+        $this->clearStudentFromDatabase($userId);
+
+        $this->studentEntityManager->persist(
+            new Student(
+                $userId,
+                $userLastName,
+                $userFirstName,
+                $userBirthDate
+            )
+        );
+
+        $this->studentEntityManager->flush();
+
+        $updatedContent = [
+            'first_name' => 'Thierry',
+            'last_name' => 'Lebon',
+            'birth_date' => '06/02/1983',
+        ];
+
+        $result = $this->subject->updateFromRequest($updatedContent, $userId);
+
+        $this->assertInstanceOf(Student::class, $result);
+        $this->assertSame($result->getLastName(), $updatedContent['last_name']);
+        $this->assertSame($result->getFirstName(), $updatedContent['first_name']);
+        $this->assertSame($result->getBirthDate()->format('d/m/Y'), $updatedContent['birth_date']);
+    }
+}
