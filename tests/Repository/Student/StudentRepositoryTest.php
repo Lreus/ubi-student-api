6cd3023c7a2d9977ec103034435f6fbb05cafa87
Lreus <ludovic.reus@free.fr>
2020-12-21 10:54:54 +0100

    Test cascading removal on student

--- tests/Repository/Student/StudentRepositoryTest.php
+++ tests/Repository/Student/StudentRepositoryTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Repository\Student;
 
+use App\Entity\Mark;
 use App\Entity\Student;
+use App\Repository\MarkRepository;
 use App\Repository\StudentRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityNotFoundException;
@@ -31,17 +33,54 @@ class StudentRepositoryTest extends AbstractStudentRepositoryTest
         $this->assertInstanceOf(Student::class, $result);
     }
 
+    public function testCascadeRemove()
+    {
+        $student = $this->getAnyStudent();
+        $this->clearStudentFromDatabase($student->getId());
+
+        foreach (['good_mark', 'bad_mark'] as $markId) {
+            $entity = $this->entityManager->find(Mark::class, $markId);
+            if (null !== $entity) {
+                $this->entityManager->remove($entity);
+            }
+        }
+        $this->entityManager->flush();
+
+        $marks = [
+            new Mark(
+                'good_mark',
+                20,
+                'grammar',
+                $student
+            ),
+            new Mark(
+                'bad_mark',
+                0.5,
+                'arts',
+                $student
+            ),
+        ];
+
+        $markRepository = self::$container->get(MarkRepository::class);
+        /** @var MarkRepository $markRepository */
+        $this->assertInstanceOf(MarkRepository::class, $markRepository);
+        $markRepository->save(...$marks);
+        $this->entityManager->clear();
+
+        $this->subject->remove($student->getId());
+        $this->assertNull($this->entityManager->find(Student::class, $student->getId()));
+
+        foreach ($marks as $mark) {
+            $this->assertNull($this->entityManager->find(Mark::class, $mark->getId()));
+        }
+    }
+
     /**
      * @covers StudentRepository::remove()
      */
-    public function testRemoveStudentRemovesEntity()
+    public function testRemoveStudentRemovesEntities()
     {
-        $student = new Student(
-            'any_id',
-            'REUS',
-            'Ludovic',
-            DateTimeImmutable::createFromFormat('d/m/Y', '07/01/1982')
-        );
+        $student = $this->getAnyStudent();
 
         $this->clearStudentFromDatabase($student->getId());
 
@@ -61,5 +100,15 @@ class StudentRepositoryTest extends AbstractStudentRepositoryTest
         $this->expectException(EntityNotFoundException::class);
 
         $this->subject->require($studentId);
+    }
+
+    private function getAnyStudent(): Student
+    {
+        return new Student(
+            'any_id',
+            'REUS',
+            'Ludovic',
+            DateTimeImmutable::createFromFormat('d/m/Y', '07/01/1982')
+        );
     }
 }
