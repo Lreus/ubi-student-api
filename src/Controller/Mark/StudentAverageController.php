f092444c56408a681a8e31e89b4490dee4f7d3c9
Lreus <ludovic.reus@free.fr>
2020-12-21 11:53:35 +0100

    Implement not found case and service delegation

---
+++ src/Controller/Mark/StudentAverageController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Mark;
+
+use App\Controller\JsonApiController;
+use App\Repository\StudentRepository;
+use App\Service\AverageMarkService;
+use Doctrine\ORM\EntityNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentAverageController extends JsonApiController
+{
+    private StudentRepository $repository;
+
+    private AverageMarkService $service;
+
+    public function __construct(StudentRepository $repository, AverageMarkService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    public function __invoke(string $studentId)
+    {
+        try {
+            $student = $this->repository->require($studentId);
+            $averageMark = $this->service->calculate($student);
+        } catch (EntityNotFoundException $exception) {
+            return $this->getJsonStandardResponse(Response::HTTP_NOT_FOUND);
+        }
+        return new Response();
+    }
+}
