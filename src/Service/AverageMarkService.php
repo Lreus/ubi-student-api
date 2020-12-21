46794834bc1374c98c15dc1cf69af2fd62e993ae
Lreus <ludovic.reus@free.fr>
2020-12-21 11:54:45 +0100

    Implement base service and test declaration for mocking purpose

---
+++ src/Service/AverageMarkService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Student;
+
+class AverageMarkService
+{
+    public function calculate(Student $student): ?float
+    {
+
+    }
+}
