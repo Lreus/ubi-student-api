8b71ac89170b2ce1bcf5948364786fcad28bf0a0
Lreus <ludovic.reus@free.fr>
2020-12-21 15:11:28 +0100

    Implement global mark average

---
+++ src/Controller/Mark/AverageController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Mark;
+
+use App\Controller\JsonApiController;
+use App\Repository\StudentRepository;
+use App\Service\AverageMarkService;
+use Symfony\Component\HttpFoundation\Response;
+
+class AverageController extends JsonApiController
+{
+    private StudentRepository $repository;
+
+    private AverageMarkService $service;
+
+    public function __construct(StudentRepository $repository, AverageMarkService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    public function __invoke()
+    {
+        $students = $this->repository->findAll();
+        $averageMark = $this->service->calculate(...$students);
+
+        if (null === $averageMark) {
+            return $this->getJsonStandardResponse(Response::HTTP_NO_CONTENT);
+        }
+
+        return $this->json(['average' => $averageMark], Response::HTTP_OK);
+    }
+}
